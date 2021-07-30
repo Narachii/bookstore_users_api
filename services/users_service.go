effd353cee2598c8e8692eef0b8565ca7f8a4130
Narachii <naoya706oxford@gmail.com>
2021-07-30 14:03:08 +0900

    refactor users service with userService struct

--- services/users_service.go
+++ services/users_service.go
@@ -5,19 +5,25 @@ import (
 	"github.com/Narachii/bookstore_users_api/utils/crypto_utils"
 	"github.com/Narachii/bookstore_users_api/utils/date_utils"
 	"github.com/Narachii/bookstore_users_api/utils/errors"
-	"log"
 )
 
-func GetUser(userId int64) (*users.User, *errors.RestErr) {
+var (
+	UsersService usersService = usersService{}
+)
+
+type usersService struct {
+
+}
+
+func (s *usersService) GetUser(userId int64) (*users.User, *errors.RestErr) {
 	result := &users.User{Id: userId}
 	if err := result.Get(); err != nil {
 		return nil, err
 	}
 	return result, nil
 }
-func CreateUser(user users.User) (*users.User, *errors.RestErr) {
+func (s *usersService) CreateUser(user users.User) (*users.User, *errors.RestErr) {
 	if err := user.Validate(); err != nil {
-		log.Println("hello")
 		return nil, err
 	}
 	user.Status = users.StatusActive
@@ -29,7 +35,7 @@ func CreateUser(user users.User) (*users.User, *errors.RestErr) {
 	return &user, nil
 }
 
-func UpdateUser (isPartial bool, user users.User)  (*users.User, *errors.RestErr) {
+func (s *usersService) UpdateUser (isPartial bool, user users.User)  (*users.User, *errors.RestErr) {
 	current := &users.User{Id: user.Id}
 	if err := current.Get(); err != nil {
 		return nil, err
@@ -59,12 +65,12 @@ func UpdateUser (isPartial bool, user users.User)  (*users.User, *errors.RestErr
 	return current, nil
 }
 
-func DeleteUser(userId int64)  *errors.RestErr{
+func (s *usersService) DeleteUser(userId int64)  *errors.RestErr{
 	user := &users.User{Id: userId}
 	return user.Delete()
 }
 
-func Search(status string)  (users.Users, *errors.RestErr){
+func (s *usersService) Search(status string)  (users.Users, *errors.RestErr){
 	dao := &users.User{}
 	return dao.FindByStatus(status)
 }
